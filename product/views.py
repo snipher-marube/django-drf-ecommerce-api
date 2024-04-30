b9326d5fda18470d4f9a4fbc70f7b6f86a8bda2e
snipher marube <sniphermarube@gmail.com>
2024-04-30 17:39:59 +0300

    viewsets and serializers

--- product/views.py
+++ product/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product, Brand, Category
+from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
+
+class CategoryViewSet(viewsets.Vieset):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class BrandViewSet(viewsets.Vieset):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class ProductViewSet(viewsets.Vieset):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)

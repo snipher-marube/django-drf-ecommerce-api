0db93e3323d76992ad4fa1be4199e9e64b81bd52
snipher marube <sniphermarube@gmail.com>
2024-05-01 09:19:02 +0300

    schema and swagger ui

--- drfecommerce/urls.py
+++ drfecommerce/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from product.views import CategoryViewSet, BrandViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register('category', CategoryViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
--- product/views.py
+++ product/views.py
@@ -1,33 +1,39 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from .models import Product, Brand, Category
 from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
 
-class CategoryViewSet(viewsets.Vieset):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+class CategoryViewSet(viewsets.ViewSet):
+   """
+   A simple ViewSet for viewing all categories.
+   """
+   queryset = Category.objects.all()
+   
+   @extend_schema(responses=CategorySerializer)
+   def list(self, request):
+       serializer = CategorySerializer(self.queryset, many=True)
+       return Response(serializer.data)
     
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-    
-class BrandViewSet(viewsets.Vieset):
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing all brands.
+    """
     queryset = Brand.objects.all()
-    serializer_class = BrandSerializer
     
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
-    
-class ProductViewSet(viewsets.Vieset):
+class ProductViewSet(viewsets.ViewSet):
+    """ 
+    A simple ViewSet for viewing all products.
+    """
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
     
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
         return Response(serializer.data)
